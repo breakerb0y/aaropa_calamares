18a3158eba1d44d276a65c8e8b922695d9b02fc9
shadichy <hotrobb360web@gmail.com>
2024-12-11 15:08:29 +0700

    Fix build script cp

--- ci/build-deb.sh
+++ ci/build-deb.sh
@@ -9,15 +9,18 @@ apt update && apt upgrade -y
 # Install debhelper
 yes | apt install -y debhelper cryptsetup pkg-kde-tools pkexec os-prober rsync git wget || :
 
-# Install dependencies
-./deps-debian11.sh
 
 current_ver=$(head -1 debian/changelog | grep -Eo '[0-9]+(\.[0-9]+){2,}')
 
 # Clone original
 wget https://github.com/calamares/calamares/archive/refs/tags/v${current_ver}.tar.gz -O - | tar -xzf -
-mv calamares-${current_ver}/* ..
-mv debian ..
+cp -rn calamares-${current_ver}/* ..
+cp -rf debian ..
+
+rm -rf debian calamares-${current_ver}
+
+# Install dependencies
+./deps-debian11.sh
 
 cd ..
 
