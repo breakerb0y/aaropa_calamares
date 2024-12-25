249056cdb07129889683cc05bfbe352f1ad56349
shadichy <hotrobb360web@gmail.com>
2024-12-25 17:03:24 +0700

    finalizing qt6 support
    Signed-off-by: shadichy <hotrobb360web@gmail.com>

--- ci/build-deb.sh
+++ ci/build-deb.sh
@@ -26,7 +26,7 @@ cp -rf debian ..
 rm -rf debian calamares-${pkgver}
 
 # Install dependencies
-yes | apt install libkf6config-dev libkf6config-dev-bin libkf6configwidgets-dev libkf6coreaddons-dev libkf6crash-dev libkf6i18n-dev libkf6iconthemes-dev libkf6kio-dev libkf6notifyconfig-dev libkf6package-dev libkf6parts-dev libkf6service-dev libkf6solid-dev libpolkit-qt6-1-dev qt6-base-private-dev qt6-declarative-dev qt6-declarative-dev-tools qt6-declarative-private-dev qt6-quick3d-dev-tools qt6-quick3dphysics-dev-tools qt6-shadertools-dev qt6-svg-dev qt6-svg-private-dev qt6-tools-dev qt6-tools-dev-tools qt6-tools-private-dev qt6-wayland-dev-tools qt6-webengine-dev qt6-webengine-dev-tools qt6-webengine-private-dev
+yes | apt install libkf6config-dev libkf6config-dev-bin libkf6configwidgets-dev libkf6coreaddons-dev libkf6crash-dev libkf6i18n-dev libkf6iconthemes-dev libkf6kio-dev libkf6notifyconfig-dev libkf6package-dev libkf6parts-dev libkf6service-dev libkf6solid-dev libpolkit-qt6-1-dev qt6-base-private-dev qt6-declarative-dev qt6-declarative-dev-tools qt6-declarative-private-dev qt6-quick3d-dev-tools qt6-quick3dphysics-dev-tools qt6-shadertools-dev qt6-svg-dev qt6-svg-private-dev qt6-tools-dev qt6-tools-dev-tools qt6-tools-private-dev qt6-wayland-dev-tools qt6-webengine-dev qt6-webengine-dev-tools qt6-webengine-private-dev cmake extra-cmake-modules libkpmcore-dev libparted-dev libpwquality-dev libboost-python-dev libyaml-cpp-dev qml6-module-qtquick-layouts qml6-module-qtquick-privatewidgets qml6-module-qtquick-window qml6-module-qtquick
 
 cd ..
 
