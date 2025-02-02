5de2d1f0e15bf106e9529f0fca1760ff01a3cfee
shadichy <hotrobb360web@gmail.com>
2025-02-02 14:58:53 +0700

    Change ci to build-deb-package action
    Signed-off-by: shadichy <hotrobb360web@gmail.com>

--- ci/debian/ceres.dockerfile
+++
@@ -1,7 +0,0 @@
-# syntax=docker/dockerfile:1-labs
-FROM devuan/migrated:ceres-slim
-
-COPY . /blissos-calamares
-WORKDIR /blissos-calamares
-
-RUN ./ci/build-deb.sh
--- ci/setup.sh
+++ ci/setup.sh
@@ -3,10 +3,8 @@
 
 cd "$(dirname "$0")"
 
-pkgname=blissos-calamares
 _ver=$(head -1 debian/changelog | grep -Eo '[0-9]+(\.[0-9]+){2,}-[0-9]+')
 pkgver=$(echo "${_ver}" | awk -F - '{print $1}')
-pkgrel=$(echo "${_ver}" | awk -F - '{print $2}')
 
 # avoid command failure
 exit_check() { [ "$1" = 0 ] || exit "$1"; }
@@ -29,15 +27,3 @@ rm -rf debian calamares-${pkgver}
 yes | apt install libkf6config-dev libkf6config-dev-bin libkf6configwidgets-dev libkf6coreaddons-dev libkf6crash-dev libkf6i18n-dev libkf6iconthemes-dev libkf6kio-dev libkf6notifyconfig-dev libkf6package-dev libkf6parts-dev libkf6service-dev libkf6solid-dev libpolkit-qt6-1-dev qt6-base-private-dev qt6-declarative-dev qt6-declarative-dev-tools qt6-declarative-private-dev qt6-quick3d-dev-tools qt6-quick3dphysics-dev-tools qt6-shadertools-dev qt6-svg-dev qt6-svg-private-dev qt6-tools-dev qt6-tools-dev-tools qt6-tools-private-dev qt6-wayland-dev-tools qt6-webengine-dev qt6-webengine-dev-tools qt6-webengine-private-dev cmake extra-cmake-modules libkpmcore-dev libparted-dev libpwquality-dev libboost-python-dev libyaml-cpp-dev qml6-module-qtquick-layouts qml6-module-qtquick-window qml6-module-qtquick
 
 cd ..
-
-# Create .orig tarball
-tar -cJf ../${pkgname}_${pkgver}.orig.tar.xz .
-
-dpkg-buildpackage -b --no-sign
-
-# export metadata
-cat <<EOF >../metadata.yml
-Name: ${pkgname}
-Version: ${_ver}
-Variants: default dbgsym
-EOF
