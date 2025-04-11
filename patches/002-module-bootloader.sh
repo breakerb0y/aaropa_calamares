2feb00726ac404cd7f2e5f16d911bce7c5b0cfb5
Shadichy <shadichy@blisslabs.org>
2025-04-11 23:20:19 +0700

    Early return right after refind-install
    Signed-off-by: Shadichy <shadichy@blisslabs.org>

--- patches/002-module-bootloader.sh
+++ patches/002-module-bootloader.sh
@@ -12,4 +12,7 @@ sed -i -r 's/("--efi-directory=" \+)( efi_directory,)/\1 installation_root_path 
 sed -i -r 's/(libcalamares\.job\.configuration\["grubCfg"\])/installation_root_path + \1/g' src/modules/bootloader/main.py
 
 # Disable use_systemd_naming
-sed -i -r 's/(use_systemd_naming).+/\1 = False/g'
+sed -i -r 's/(use_systemd_naming).+/\1 = False/g' src/modules/bootloader/main.py
+
+# Return right after refind-install to prevent non-related configurations
+sed -i -r 's/(\s+)(check_target_env_call\(\["refind-install"\]\))/\1\2\n\1open(conf_path, "w")\n\1return/g' src/modules/bootloader/main.py
